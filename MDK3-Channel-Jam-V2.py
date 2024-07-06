d33c194565588280acb9290000c4a71694ff08f7
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-07-06 21:32:19 +0200

    Update MDK3-Channel-Jam-V2.py
    Fully Working Now

--- MDK3-Channel-Jam-V2.py
+++ MDK3-Channel-Jam-V2.py
@@ -7,13 +7,13 @@ time.sleep(1)
 os.system("clear")
 
 # Print banner
-print "-" * 60
+print "-" * 70
 print "          [ - MDK3 Channel Jammer - ]"
 print "! Code was created by Fattcat for Tp_Link Archer C7 v5 personal usage !"
 print "Checkout my GitHub Repo --> github.com/Fattcat"
 print "Checkout my YouTube --> https://www.youtube.com/channel/UCKfToKJFq-uvI8svPX0WzYQ"
 print "! DONT USE THIS CODE FOR BAD HACKING or WITHOUT PERMISSIONS !"
-print "-" * 60 + "\n"
+print "-" * 70 + "\n"
 
 print "Checking if script was started with sudo ..."
 
